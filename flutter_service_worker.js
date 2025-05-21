5bd0966365ba31574f75f50541841c62b134ba9a
JinMoriDann <caio.pariz2000@gmail.com>
2025-05-21 19:24:18 -0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3bb795a7d37ffb511e3221a96c24eb74
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "4c337fb70fb0544ed492a2f1fa82c604",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "48750e71684a3470fcd2d4146018b52d",
+"flutter_bootstrap.js": "7dc76581492402001600ac6123ae3ab3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
